f1f4bf5231e332e5c673c7f299d9ccde48e7a473
Anomalia4 <seruy4@gmail.com>
2018-02-07 18:10:59 +0200

    Initial commit

---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,69 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class LoginPage {
+    private WebDriver driver;
+   // private WebDriverWait wait;
+    public LoginPage(WebDriver webDriver){
+        driver = webDriver;
+       // wait = new WebDriverWait(driver, 3, 500);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "71:2;a")
+    private WebElement usernameField;
+
+    @FindBy(id = "81:2;a")
+    private WebElement passwordField;
+
+    @FindBy(id = "93:2;a")
+    private WebElement remeberMeCheckbox;
+
+    @FindBy(xpath = "//input[@id='103:2;a' and @class='uiInput uiInputCheckbox uiInput--default uiInput--checkbox']")
+    private WebElement iAcceptTermsAndConditionsCheckbox;
+
+    @FindBy(linkText = "Terms and Conditions")
+    private WebElement termsAndConditionsLink;
+
+    @FindBy(linkText = "Forgot your password?")
+    private WebElement forgotYourPasswordLink;
+
+    @FindBy(css = "button.slds-button.slds-button--brand.pmp_login_button.uiButton--default.uiButton")
+    WebElement loginButton;
+
+    @FindBy(xpath = "//h4[contains(text(), 'Your login attempt has failed. Make sure the username and password are correct.')]")
+    WebElement incorrectUsernameOrPasswordMessage;
+
+    public void setUsername(String username){
+        usernameField.clear();
+        usernameField.sendKeys(username);
+    }
+
+    public void setPassword(String password){
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton(){
+        loginButton.click();
+    }
+
+    public void clickRememberMe(){
+        remeberMeCheckbox.click();
+    }
+
+    public void clickIAcceptTermsAndConditions(){
+        iAcceptTermsAndConditionsCheckbox.click();
+    }
+
+    public boolean incorrectUsernameOrPasswordMessageIsDisplayed(){
+        return (incorrectUsernameOrPasswordMessage.isDisplayed());
+    }
+
+   /* public void waitingForLoadingElementByClassName(String className){
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className(className)));
+    } */
+}
---
+++ src/test/java/LoginPageTestClass.java
@@ -0,0 +1,59 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPageTestClass {
+    private WebDriver driver = new ChromeDriver();
+    private LoginPage loginPage = new LoginPage(driver);
+    private WebDriverWait wait = new WebDriverWait(driver, 4, 800);
+
+    @Before
+    public void setup(){
+        driver.get("https://preprod-smartbox.cs86.force.com/s/login/?language=en_GB");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("71:2;a")));
+    }
+
+    @Test
+    public void loginWithWrongUsername(){
+        loginPage.setUsername("wrong.username@test.com.preprod.community");
+        loginPage.setPassword("tHXBqJgrmSSbhItN3cRF");
+        loginPage.clickIAcceptTermsAndConditions();
+        loginPage.clickLoginButton();
+        wait.until(ExpectedConditions.visibilityOf(loginPage.incorrectUsernameOrPasswordMessage));
+        Assert.assertTrue("Incorrect login error message is not displayed", loginPage.incorrectUsernameOrPasswordMessageIsDisplayed());
+    }
+
+    @Test
+    public void loginWithWrongPassword(){
+        loginPage.setUsername("sergey.karpenko@pexlify.com.preprod.community");
+        loginPage.setPassword("Wrong_Password");
+        loginPage.clickIAcceptTermsAndConditions();
+        loginPage.clickLoginButton();
+        wait.until(ExpectedConditions.visibilityOf(loginPage.incorrectUsernameOrPasswordMessage));
+        Assert.assertTrue("Incorrect password error message is not displayed", loginPage.incorrectUsernameOrPasswordMessageIsDisplayed());
+    }
+
+
+    @Test
+    public void loginToSalesforce(){
+        loginPage.setUsername("sergey.karpenko@pexlify.com.preprod.community");
+        loginPage.setPassword("tHXBqJgrmSSbhItN3cRF");
+        //loginPage.clickRememberMe();
+        loginPage.clickIAcceptTermsAndConditions();
+        loginPage.clickLoginButton();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("navigationMenuWrapper")));
+        Assert.assertTrue("Partner should be redirected to Home page after log in", driver.getCurrentUrl().contains("smartbox.cs86.force.com/s/"));
+    }
+
+    @After
+    public void teardown(){
+        if(driver != null)
+        driver.quit();
+    }
+}
