cf751a85134f9761635e1281ae822451cb632df6
Anomalia4 <seruy4@gmail.com>
2018-03-01 18:15:55 +0200

    External class for keeping credentials was created

---
+++ src/test/java/OrganizationCredentials.java
@@ -0,0 +1,11 @@
+public class OrganizationCredentials {
+    private static final String preprodCommunityUsername = "sergey.karpenko@pexlify.com.preprod.community";
+    private static final String preprodCommunityPassword = "tHXBqJgrmSSbhItN3cRF";
+    private static final String organizationUsername = "seruy4@gmail.com";
+    private static final String organizationPassword = "1234qwerASDF";
+
+    static String getUsername(){
+        
+    }
+
+}
